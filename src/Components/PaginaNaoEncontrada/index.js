a14eafd6dd630feba8ff21a2cf103fee72d7171f
Caio Souza <souzsdev@gmail.com>
2022-05-23 14:07:12 -0300

    Página não encontrada para outras rotas

---
+++ src/Components/PaginaNaoEncontrada/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Titulo } from '../../globalStyles'
+
+const PaginaNaoEncontrada = () => {
+  return (
+    <Container>
+        <Titulo>Página não encontrada</Titulo>
+    </Container>
+  )
+}
+
+export default PaginaNaoEncontrada
