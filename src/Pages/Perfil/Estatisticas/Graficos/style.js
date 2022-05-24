73ad2e70b4446524fbeeb63320954be4fe9a3523
Caio Souza <souzsdev@gmail.com>
2022-05-24 09:17:50 -0300

    Página de estatísticas com utilização da biblioteca victory

---
+++ src/Pages/Perfil/Estatisticas/Graficos/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Container } from '../../../../globalStyles';
+
+export const ContainerGrid = styled(Container)`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 4rem 2rem;
+    margin-bottom: 4rem;
+`;
+
+export const Acessos = styled.div`
+    padding: 2rem;
+    font-size: 2rem;
+`;
